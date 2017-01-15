f2d7e01deb53b7bba389fb5831ba36e22f4a7346
Phát <cedrichoang@gmail.com>
2017-01-16 01:19:48 +0700

    Create article model and validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170109052616) do
+ActiveRecord::Schema.define(version: 20170115174742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "identities", force: :cascade do |t|
     t.integer  "user_id"
