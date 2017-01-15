3cf9dd6479d427e08d9b9249dc1b8dd4617cd1c0
Phát <cedrichoang@gmail.com>
2017-01-16 01:56:31 +0700

    Create new template, action and route

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'home/index'
   root to: "home#index"
+
+  resources :articles
 end
